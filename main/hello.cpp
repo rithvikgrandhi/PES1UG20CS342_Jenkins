e1440c843a0de8d881f25f459d22f7c97a6ac5bf
Rithvik <grandhirithwik@gmail.com>
2023-02-16 18:42:49 +0530

    added files

---
+++ main/hello.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+
+  cout<<  "PES1UG20CS342\n";
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
