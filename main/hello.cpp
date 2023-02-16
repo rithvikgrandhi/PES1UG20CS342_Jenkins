cdfbde2518c3724d724b38d987829d0ca37fcfe8
Rithvik <grandhirithwik@gmail.com>
2023-02-16 19:30:12 +0530

    made changes to the cpp file

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,9 +4,10 @@ using namespace std;
 
 int main(void) {
 
-  cout<<  "PES1UG20CS342\n";
+  cout << "PES1UG20CS342\n";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "This is with the changes made to the hello.cpp file"
   return 0;
 }
